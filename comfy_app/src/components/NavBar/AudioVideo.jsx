dc1eefcc6985572fcac363065d9e7e4023db68da
Jin Choo <jinchoo315@gmail.com>
2022-10-14 13:59:50 -0500

    fix prettier error

--- comfy_app/src/components/NavBar/AudioVideo.jsx
+++ comfy_app/src/components/NavBar/AudioVideo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export default function AudioVideo() {
   return (
--- comfy_app/src/components/NavBar/NavBar.jsx
+++ comfy_app/src/components/NavBar/NavBar.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import { Routes, Route, Link } from "react-router-dom";
-import About from "./About";
-import Contact from "./Contact";
-import SignUp from "./SignUp";
-import AudioVideo from "./AudioVideo";
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Routes, Route, Link } from 'react-router-dom';
+import About from './About';
+import Contact from './Contact';
+import SignUp from './SignUp';
+import AudioVideo from './AudioVideo';
 
 function NavBar() {
   return (
     <>
-      <div className="NavBar">
-        {["sm"].map((expand) => (
-          <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+      <div className='NavBar'>
+        {['sm'].map((expand) => (
+          <Navbar key={expand} bg='light' expand={expand} className='mb-3'>
             <Container fluid>
-              <Navbar.Brand href="/about">Comfy</Navbar.Brand>
+              <Navbar.Brand href='/about'>Comfy</Navbar.Brand>
               <Navbar.Toggle
                 aria-controls={`offcanvasNavbar-expand-${expand}`}
               />
               <Navbar.Offcanvas
                 id={`offcanvasNavbar-expand-${expand}`}
                 aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                placement="end"
+                placement='end'
               >
                 <Offcanvas.Header closeButton>
                   <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
@@ -31,17 +31,17 @@ function NavBar() {
                   </Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                  <Nav className="justify-content-end flex-grow-1 pe-3">
-                    <Nav.Link as={Link} to="/about">
+                  <Nav className='justify-content-end flex-grow-1 pe-3'>
+                    <Nav.Link as={Link} to='/about'>
                       About
                     </Nav.Link>
-                    <Nav.Link as={Link} to="/contact">
+                    <Nav.Link as={Link} to='/contact'>
                       Contact
                     </Nav.Link>
-                    <Nav.Link as={Link} to="/signUp">
+                    <Nav.Link as={Link} to='/signUp'>
                       SignUp
                     </Nav.Link>
-                    <Nav.Link as={Link} to="/audioVideo">
+                    <Nav.Link as={Link} to='/audioVideo'>
                       Audio/Video
                     </Nav.Link>
                   </Nav>
@@ -53,10 +53,10 @@ function NavBar() {
       </div>
       <div>
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/audioVideo" element={<AudioVideo />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/signUp' element={<SignUp />} />
+          <Route path='/audioVideo' element={<AudioVideo />} />
         </Routes>
       </div>
     </>
