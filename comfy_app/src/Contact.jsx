073e0ceec0cf10aa4f4a3d28843e2e0227fb1091
Tommy Dang <ledangt310@live.com>
2022-10-18 18:44:44 -0700

    add contact page

--- comfy_app/src/Contact.jsx
+++ comfy_app/src/Contact.jsx
@@ -1,62 +1,50 @@
-import React, { useState } from "react";
-import "./index.css";
+import React, { useState } from 'react';
+import './index.css';
 
-const Contact = () => {
-  const [form, setForm] = useState("Submit");
-
-  const onFormSubmit = (e) => {
-    e.preventDefault();
-    setForm("Submitting ...");
-    const { firstName, lastName, email, message } = e.target.elements;
-    let contactForm = {
-      firstName: firstName.value,
-      lastName: lastName.value,
-      email: email.value,
-      message: message.value,
-    };
-  };
+function Contact() {
+  const [form, setForm] = useState('Submit');
   return (
-    <div className="container">
-      <h2 className="mb-3">Contact Us</h2>
-      <h6 className="mb-3">We will get back to you as soon as possible</h6>
-      <form onSubmit={onFormSubmit}>
-        <div className="card card-body col-md-6 mx-auto align-self-center">
-          <div className="mb-3">
-            <label className="form-label d-flex flex-row" htmlFor="firstName">
+    <div className='container'>
+      <h2 className='mb-3'>Contact Us</h2>
+      <h6 className='mb-3'>We will get back to you as soon as possible</h6>
+      <form>
+        <div className='card card-body col-md-6 mx-auto align-self-center'>
+          <div className='mb-3'>
+            <label className='form-label d-flex flex-row' htmlFor='firstName'>
               First Name
             </label>
             <input
-              className="form-control"
-              type="text"
-              id="firstName"
+              className='form-control'
+              type='text'
+              id='firstName'
               required
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label d-flex flex-row" htmlFor="lastName">
+          <div className='mb-3'>
+            <label className='form-label d-flex flex-row' htmlFor='lastName'>
               Last Name
             </label>
             <input
-              className="form-control"
-              type="text"
-              id="lastName"
+              className='form-control'
+              type='text'
+              id='lastName'
               required
             />
           </div>
-          <div className="mb-3">
-            <label className="form-label d-flex flex-row" htmlFor="email">
+          <div className='mb-3'>
+            <label className='form-label d-flex flex-row' htmlFor='email'>
               Email
             </label>
-            <input className="form-control" type="email" id="email" required />
+            <input className='form-control' type='email' id='email' required />
           </div>
-          <div className="mb-3">
-            <label className="form-label d-flex flex-row" htmlFor="message">
+          <div className='mb-3'>
+            <label className='form-label d-flex flex-row' htmlFor='message'>
               Message
             </label>
-            <textarea className="form-control" id="message" rows="4" required />
+            <textarea className='form-control' id='message' rows='4' required />
           </div>
-          <div className="d-grid gap-2 d-md-flex justify-content-md-left">
-            <button className="btn btn-info " type="submit">
+          <div className='d-grid gap-2 d-md-flex justify-content-md-left'>
+            <button className='btn btn-info ' type='submit'>
               {form}
             </button>
           </div>
