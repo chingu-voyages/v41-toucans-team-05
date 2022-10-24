220e831f15856332b63c133c5361832989360646
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-10-23 22:34:00 -0500

    changing directory name

--- comfy_app/src/components/Weatherapi/WeatherApi.jsx
+++ comfy_app/src/components/Weatherapi/WeatherApi.jsx
@@ -2,7 +2,7 @@ import '../../App.css';
 import React, { useState, useEffect } from 'react';
 import Moment from 'react-moment';
 import axios from 'axios';
-import Showtime from '../liveDateAndTime/Showtime';
+import Showtime from '../liveTime/Showtime';
 import useGeoLocation from '../../hooks/useGeolocation';
 
 function WeatherApi() {
