c7b950805236df9be33e5adf81e28ae8d9f1b697
Karishtan <52981138+Karishtan@users.noreply.github.com>
2022-10-26 19:28:29 -0700

    add footer component to about

--- comfy_app/src/components/NavBar/About.jsx
+++ comfy_app/src/components/NavBar/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import WeatherApi from '../Weatherapi/WeatherApi';
+import Footer from '../Footer/Footer';
 
 export default function About() {
   return (
     <div>
       <WeatherApi />
+      <Footer />
     </div>
   );
 }
