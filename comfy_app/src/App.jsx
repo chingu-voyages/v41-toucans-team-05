0d5200664145228b328b827c5b13f6d5670d1d43
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2022-10-29 20:55:48 -0500

    removed

--- comfy_app/src/App.jsx
+++ comfy_app/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
-import WeatherApi from './components/Weatherapi/WeatherApi';
-// import Footer from './components/Footer/Footer';
-import FooterTest from './components/Footer/FooterTest';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
-      <FooterTest />
+      <Footer />
     </div>
   );
 }
